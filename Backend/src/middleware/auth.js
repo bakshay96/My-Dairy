28398c3cc3f48fcf61b3a49191a8d69bad07862d
bakshay96 <akshaymali.edu@gmail.com>
2024-01-13 14:43:09 +0530

    update

--- Backend/src/middleware/auth.js
+++ Backend/src/middleware/auth.js
@@ -2,9 +2,11 @@ const jwt = require("jsonwebtoken")
 require("dotenv").config();
 
 const auth=(req,res,next)=>{
-const token=req.headers.authorization
-if(token){
+const token=req.headers.authorization;
+//console.log("token",token,req.headers)
+if(token !==""){
     try{
+        console.log("if token",token)
         const decoded= jwt.verify(token.split(" ")[1],process.env.TOKEN_API_SECRET_KEY);
         if(decoded){
             next();
