e91c86b3994f434100ce4f0abacd6bab88f05107
bakshay96 <akshaymali.edu@gmail.com>
2024-06-03 13:16:24 +0530

    update

--- Frontend/src/Redux/Api/api.js
+++ Frontend/src/Redux/Api/api.js
@@ -1,4 +1,2 @@
-export  const  url = "https://milkify.cyclic.app/api";
-export  const localhost ="http://localhost:8080/api" ;
-//export  const url2 ="https://my-dairy-e7as.onrender.com/api";
+
 export  const url2 ="http://localhost:8080/api" ;
