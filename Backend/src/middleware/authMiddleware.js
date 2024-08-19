8c5fafc49617f4bf1579c168b751fd1c0bf0456b
bakshay96 <akshaymali.edu@gmail.com>
2024-08-19 20:40:54 +0530

    update routes

--- Backend/src/middleware/authMiddleware.js
+++ Backend/src/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const { AdminModel } = require('../Admin/admin.model');
 
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.header('Authorization')?.split(' ')[1];
+    //const token = req.header('Authorization')?.split(' ')[1];
     console.log("token",token)
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
@@ -14,7 +14,7 @@ const authMiddleware = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.admin = await AdminModel.findById(decoded.id).select('-password');
-        console.log("admin",req.admin,"id",req.admin.id)
+        //console.log("admin",req.admin,"id",req.admin.id)
         next();
     } catch (err) {
         res.status(401).json({ msg: 'Token is not valid' });
