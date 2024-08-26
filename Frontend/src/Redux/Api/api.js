2290b16c1c558552306c7c3ba881ae4cbea0e519
bakshay96 <akshaymali.edu@gmail.com>
2024-08-26 12:50:06 +0530

    update app doc

--- Frontend/src/Redux/Api/api.js
+++ Frontend/src/Redux/Api/api.js
@@ -1,10 +1,6 @@
-export  const  url = "https://milkify.cyclic.app/api";
-export  const localhost ="http://localhost:8080/api" ;
-export  const url2 ="https://my-dairy-e7as.onrender.com/api";
-//export  const url2 ="http://localhost:8080/api" ;
 
 // export  const  url = process.env.REACT_APP_API_URL_cyclic;
 // export  const localhost =process.env.REACT_APP_API_URL_localhost ;
-// export  const url2 =process.env.REACT_APP_API_URL_render;
+ export  const url2 =process.env.REACT_APP_API_URL_render;
 // export  const url2 =process.env.REACT_APP_API_URL_localhost ;
 // console.log('api url',url2);
