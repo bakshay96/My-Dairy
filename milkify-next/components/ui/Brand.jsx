284d883aa5e332ff43b588b6f846aa24968803f3
bakshay96 <akshaymali.edu@gmail.com>
2026-04-29 07:40:21 +0530

    update brand logo path

--- milkify-next/components/ui/Brand.jsx
+++ milkify-next/components/ui/Brand.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-
+import brandLogo from "../../public/images/milkify-logo.png"
 export default function Brand({ compact = false, className = "" }) {
   return (
     <span className={cn("inline-flex items-center gap-2", className)}>
       <span className="inline-flex h-9 w-9 items-center justify-center rounded-xl overflow-hidden ring-1 ring-primary/10">
         <Image 
-          src="/images/milkify-logo.png" 
+          src={brandLogo} 
           alt="Milkify Logo" 
           width={36} 
           height={36} 
